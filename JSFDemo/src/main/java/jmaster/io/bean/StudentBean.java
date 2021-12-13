a83d54d1c6a11aba3e47d04afc310320c731a1d6
JMaster <dinhduccuong1989@gmail.com>
2021-12-13 11:40:54 +0700

    update report jasper

--- JSFDemo/src/main/java/jmaster/io/bean/StudentBean.java
+++ JSFDemo/src/main/java/jmaster/io/bean/StudentBean.java
@@ -1,13 +1,23 @@
 package jmaster.io.bean;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 
 import jmaster.io.dao.StudentDao;
 import jmaster.io.entity.Student;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @ManagedBean
 public class StudentBean implements Serializable {
@@ -86,5 +96,45 @@ public class StudentBean implements Serializable {
 		Student student = studentDao.getId(id);
 		FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("editStudent", student);
 		return "/student/edit-student.xhtml?faces-redirect=true";
+	}
+
+	public void downloadReport() {
+		try {
+			HashMap<String, Object> mapParams = new HashMap<>();
+			mapParams.put("name", "Test report");
+
+			JRBeanCollectionDataSource beanCollectionDataSource = new JRBeanCollectionDataSource(getStudents());
+			JasperPrint jasperPrint = JasperFillManager.fillReport(getFileFromResourceAsStream("report/student.jasper"),
+					mapParams, beanCollectionDataSource);
+
+			FacesContext facesContext = FacesContext.getCurrentInstance(); // Get the context ONCE
+			HttpServletResponse response = (HttpServletResponse) facesContext.getExternalContext().getResponse();
+		    ServletOutputStream servletOutputStream = response.getOutputStream();
+			response.addHeader("Content-disposition", "attachment; filename=report.pdf");
+			facesContext.responseComplete();
+
+			JasperExportManager.exportReportToPdfStream(jasperPrint, servletOutputStream);
+
+			servletOutputStream.flush();
+		    servletOutputStream.close();
+		} catch (JRException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// get a file from the resources folder
+	// works everywhere, IDEA, unit test and JAR file.
+	private InputStream getFileFromResourceAsStream(String fileName) {
+		// The class loader that loaded the class
+		ClassLoader classLoader = getClass().getClassLoader();
+		InputStream inputStream = classLoader.getResourceAsStream(fileName);
+
+		// the stream holding the file content
+		if (inputStream == null) {
+			throw new IllegalArgumentException("file not found! " + fileName);
+		} else {
+			return inputStream;
+		}
+
 	}
 }
